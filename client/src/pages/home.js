c5fd7130bda2db6ece2d73817a4ada397fb9ad0a
Mayur <mayurbehere7432@gmail.com>
2024-03-11 20:50:53 +0530

    Beta Chat Page Ready

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import "./styles/home.css";
+import "../styles/home.css";
 import LocomotiveScroll from "locomotive-scroll";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import "font-awesome/css/font-awesome.min.css";
