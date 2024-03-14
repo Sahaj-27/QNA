cf53dbefe8e16f6950a7a6aae2da090657a20e78
Mayur <mayurbehere7432@gmail.com>
2024-03-15 02:48:28 +0530

    Fixed Security Issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
@@ -8,28 +8,27 @@ import Forgot from "./pages/forgot";
 import Bot from "./pages/bot";
 import { Toaster } from "react-hot-toast";
 import { UserInfoContextProvider } from "./context/UserInfoContext";
-import ProtectedRoute from "./context/ProtectedRoutes";
+import ProtectedRoutes from "./context/ProtectedRoutes";
 
 // import CustomCursor from "./components/CustomCursor";
 
 function App() {
-  return (
-    <UserInfoContextProvider>
-    <div className="App">
-      <Toaster  />
-      <Router>
-        <Switch>
-          <ProtectedRoute path="/bot-chat" element={<Bot />} />
-          <ProtectedRoute path="/QnA-Input" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot" element={<Forgot />} />
-          <Route path="/" element={<Home />} />
-          </Switch>
-      </Router>
-    </div>
-    </UserInfoContextProvider>
-  );
+	return (
+		<UserInfoContextProvider>
+			<div className="App">
+				<Toaster />
+				<Router>
+					<Routes>
+						<Route path="/login" element={<Login />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/forgot" element={<Forgot />} />
+						<Route path="/" element={<Home />} />
+						<Route path="*" element={<ProtectedRoutes />} />
+					</Routes>
+				</Router>
+			</div>
+		</UserInfoContextProvider>
+	);
 }
 
 export default App;
